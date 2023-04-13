e0e4b24961388b259bb42b326f2cc449358e1f86
PhoenixFire <72724975+help-programming-is-hard@users.noreply.github.com>
2023-04-13 12:27:48 -0700

    some changes

--- BCA Complete.py
+++ BCA Complete.py
@@ -6,7 +6,14 @@ import math
 def clear():
   os.system('cls' if os.name == 'nt' else 'clear')
 
-class BCATable:
+def parse_command(commandString):
+  for char in commandString:
+    if char == " ":
+      return commandString.split(" ")
+    else:
+      return commandString
+
+# class BCATable:
 
 # BCA Tables will be represented as a class with multiple lists
 
@@ -17,5 +24,28 @@ class BCATable:
 # Each Reactant/Product will have a Before, Change, and After list
 # compounds are from the periodictable library
 
-
-
+while (True):
+  clear()
+  print("BCA Complete - By Matthew Dinh")
+  print("Type exit at any point to exit the command or program.\n")
+  
+  command = input("Enter a command [Ex: help, exit]: ")
+  if command == "help":
+    print("")
+    print("help - Displays this help message")
+    print("new - Creates a new BCA table, and deletes the current one")
+    print("set compound - Requires a compound and a reaction/product index. Sets a reactant or product in the reaction.")
+    print("set compound amount - Requires a  - Displays the mass of a compound in g/mol")
+    print("view - Displays the current BCA table")
+    
+    print("exit - Exits the program")
+    print("")
+    input("Press enter to continue...")
+  if command == "convert":
+    print("")
+    convert()
+  if command == "mass":
+    print("")
+    mass()
+  if command == "exit":
+    break
